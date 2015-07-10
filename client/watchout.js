7bb505fd564217e58ae58c6141bfb3d5c2b1b76b
sara-gudeman <sgudeman982@gmail.com>
2015-07-10 15:53:32 -0700

    made gameboard, implemented enemy function

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,88 @@
 // start slingin' some d3 here.
 
+// set up function
+// -- provide board
+// -- provide group container
+// -- create enemies (initial number)
+// -- place player (randomly maybe)
+// -- initialize & set timer
+
+// player module
+// -- methods (player actions)
+
+// enemy module
+// -- create enemies
+// -- methods (enemy actions)
+
+// enemy movement module
+// -- check all enemies
+// -- monitor positions
+
+// collision detection module
+
+// player class
+// -- create new player
+
+
+
+// setup
+
+var height = 400,
+    width = 400;
+
+var makeRandomNum = function(factor) {
+  return Math.floor(Math.random() * factor);
+}
+
+// Player constructor function
+var Player = function(x, y, type) {
+  this.body;
+  this.x = x || 0;
+  this.y = y || 0;
+  this.type = type || "enemy";
+};
+
+// game board selector
+var board = d3.select('.gameboard').append('svg')
+    .attr('width', width)
+    .attr('height', height)
+  .append('g');
+
+// draw a player on the gameboard
+var drawPlayer = function(cx, cy) {   
+  board.append('circle')
+    .attr('cx', cx)
+    .attr('cy', cy)
+    .attr('r', 20)
+    .attr('fill', 'red')
+};
+
+// create some enemies for game
+var makeEnemies = function(num) {
+  for (var i = 0; i < num; i++) {
+    drawPlayer(makeRandomNum(width), makeRandomNum(height));
+  }
+}
+
+makeEnemies(4);
+var player = new Player(width / 2, height / 2, 'friend');
+
+// d3.select('.gameboard')
+//     .data([player], function(d){return [player]})
+//     // .data(player)
+//     // .enter()
+//     // .append('svg')
+//     //   .attr('width', 40)
+//     //   .attr('height', 40)
+//       .append('circle')
+//         .attr('cx', 20)
+//         .attr('cy', 20)
+//         .attr('r', 20)
+//         .attr('fill', 'red');
+
+// d3.select('.gameboard')
+//     .data([player], function(d){return [player]})
+//     .attr('width', 100);
+
+
+  
